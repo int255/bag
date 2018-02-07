56db5a8d4d1a6ae051fb7b3144f04955f41f4b15
int255 <int255@outlook.com>
2018-02-07 13:24:20 +0800

    update bag.cpp

--- bag.cpp
+++ bag.cpp
@@ -2,106 +2,131 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <cassert>
+#include <type_traits>
 
 struct item_t
 {
-	int weight;
-	int value; 
+    char id;
+    int weight;
+    int value;
 };
 
 struct imp_t : public item_t
 {
-	bool can_choose;
-	bool chosen;
+    bool can_choose;
+    bool chosen;
 };
 
-static int sLevel = 0;
-int fill(int capacity, std::vector<imp_t> & imps)
+void inspect(const std::vector<imp_t> & imps, int * outTotalValue, int * usedCapacity, std::string * outStr = nullptr)
 {
-	++sLevel;
+    int total_value = 0;
+    int used_capacity = 0;
+    char buf[256];
+    std::string str;
+    for (int i = 0; i<imps.size(); ++i)
+    {
+        if (imps[i].chosen)
+        {
+            total_value += imps[i].value;
+            used_capacity += imps[i].weight;
+            str.append("Y");
+        }
+        sprintf(buf, "(%c v:%d, w:%d)", imps[i].id, imps[i].value, imps[i].weight);
+        str.append(buf);
+        str.append(", ");
+    }
+    
+    if (outTotalValue)
+        *outTotalValue = total_value;
+    
+    if (usedCapacity)
+        *usedCapacity = used_capacity;
+    
+    if (outStr)
+        *outStr = str;
+}
 
-	int value = 0;
+std::vector<char> init_tags()
+{
+    std::vector<char> tags;
+    for ( char ch = 'A'-1; ch<='Z'; ++ch)
+    {
+        tags.push_back(ch);
+    }
+    return tags;
+}
 
-	auto it = std::find_if(imps.begin(), imps.end(), [](const imp_t & imp){
-		return imp.can_choose;
-	});
+static int sLevel = 0;
+static std::vector<char> sTags = init_tags();
+static int k = 0;
 
-	if ( imps.end() == it)
-	{
-		value = 0;
-	} else {
 
-		int v = 0;
-		{
-			// skip item
-			it->can_choose = false;
-			int cap1 = capacity;
-			int v1 = fill(cap1, imps);
-
-			it->chosen = true;
-			v = it->value;
-			int cap2 = capacity - it->weight;
-			int v2 = 0;
-			if (cap2 >= 0)
-			{
-				v2 = fill(cap2, imps);
-			}
-			
-			bool skip = (v2 + it->value) < v1;
-			if ( skip )
-			{
-				it->can_choose = false;
-				it->chosen = false;
-				value += v1;
-			} else {
-				it->chosen = true;
-				it->can_choose = false;
-				value += v2;
-			}
-		}
-	}
-	--sLevel;
-
-	std::string str;
-	char buf[256];
-	for (int i = 0; i<imps.size(); ++i)
-	{
-		if (imps[i].chosen)
-		{
-			sprintf(buf, "(i:%d, v:%d, w: %d),", i, imps[i].value, imps[i].weight);
-			str.append(buf);
-		}
-	}
-	printf("lvl: %d, total_value:%d, chosen:%s\n", sLevel, value, str.c_str());
-	return value;
+// return the max value
+int fill(int capacity, const std::vector<item_t> & items, std::vector<char> & choices)
+{
+    auto vi = items;
+    // ran out of items
+    if (items.empty())
+        return 0;
+    
+    auto it = std::find_if(vi.begin(), vi.end(), [capacity](const item_t & item){
+        return item.weight <capacity;
+    });
+    
+    // ran out of capacity
+    if (it == vi.end())
+        return 0;
+    
+    assert(it!=vi.end());
+    vi.erase(it);
+    char item_id = it->id;
+    int item_value = it->value;
+    // take it
+    std::vector<char> choices1;
+    int value1 = fill(capacity - it->weight, vi, choices1);
+    // don't take it
+    std::vector<char> choices2;
+    int value2 = fill(capacity, vi, choices2);
+    
+    if (value1 + item_value < value2 )
+    {
+        // honor 2
+        choices = choices2;
+        return value2;
+    } else {
+        // honor 1
+        choices = choices1;
+        choices.push_back(item_id);
+        return value1 + item_value;
+    }
 }
 
 void fill_bag(int capacity, const std::vector<item_t> & items)
 {
-	std::vector<imp_t> imps;
-	for ( auto & item : items)
-	{
-		imp_t imp;
-		imp.weight = item.weight;
-		imp.value = item.value;
-		imp.can_choose = true;
-		imp.chosen = false;
-		imps.emplace_back(std::move(imp));
-	}
-	fill(capacity, imps);
+    static_assert(std::is_pod<item_t>::value, "item_t must be pod");
+    std::vector<char> choices;
+    auto value = fill(capacity, items, choices);
+    printf("max value: %d, choices:", value);
+    std::for_each(choices.begin(), choices.end(), [](char ch){
+        printf("%c ", ch);
+    });
+    printf("\n");
+
 }
 
 int main()
 {
-	std::vector<item_t> items = {
-		{1, 1},
-		{2, 3},
-		{10, 6},
-		{5, 8},
-		{3, 5},
-	};
-
-	int capacity = 15;
-	fill_bag(capacity, items);
-	return 0;
+    std::vector<item_t> items = {
+        {'a', 1, 1},
+        {'b', 2, 6},
+        {'c', 5, 18},
+        {'d', 6, 22},
+        {'e', 7, 28},
+    };
+    
+    int capacity = 11;
+    fill_bag(capacity, items);
+    return 0;
 }
+
