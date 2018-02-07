cfaedd67acb82795dfd8d23a3d78742bfe0ba20c
int255 <int255@outlook.com>
2018-02-07 15:51:43 +0800

    update bag.cpp

---
+++ bag/aaa/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  aaa
+//
+//  Created by Xu, Neil on 06/02/2018.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
